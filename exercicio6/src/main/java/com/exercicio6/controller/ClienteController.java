7fcabc83b7eb7f7f1709ce9425d98abf7eabb659
matheus1519 <1519academico@gmail.com>
2019-12-09 21:27:05 -0300

    Subir e Mostrar Imagem

--- exercicio6/src/main/java/com/exercicio6/controller/ClienteController.java
+++ exercicio6/src/main/java/com/exercicio6/controller/ClienteController.java
@@ -2,6 +2,10 @@ package com.exercicio6.controller;
 
 import com.exercicio6.model.Cliente;
 import com.exercicio6.repository.ClienteRepository;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
+import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,6 +26,9 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clientes;
     
+    @Autowired
+    ServletContext context;
+    
     @GetMapping
     public ModelAndView listar() {
         ModelAndView mv = new ModelAndView("ListarClientes");
@@ -27,10 +36,29 @@ public class ClienteController {
         return mv;
     }
     
-    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST,  consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String cadastrar(Cliente cliente) {
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+    public ModelAndView cadastrar(@RequestParam("file") MultipartFile file, Cliente cliente) {
+        ModelAndView mv = new ModelAndView("CadastrarCliente");
+        
+        if(file.isEmpty()){
+            mv.addObject("msgFile", "Foto não carregada!");
+            return mv;
+        }
+        
+        try{
+            String name = Calendar.getInstance().getTimeInMillis()+file.getOriginalFilename();
+            file.transferTo(Paths.get("C:\\Users\\1519m\\Pictures\\"+name));
+            mv.addObject("path", context.getContextPath() +"/images/"+name);
+            cliente.setFoto("http://localhost:8080/images/"+name);
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+        
+        
         clientes.save(cliente);
-        return "redirect:/clientes";
+        return mv;
+//        return "redirect:/clientes";
     }
     
     @GetMapping("/cadastrar")
@@ -39,5 +67,8 @@ public class ClienteController {
         mv.addObject("cliente",new Cliente());
         return mv;
     }
+    
+        
+    
     
 }
---
+++ exercicio6/src/main/java/com/exercicio6/controller/ImageController.java
@@ -0,0 +1,37 @@
+package com.exercicio6.controller;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ImageController {
+
+    @RequestMapping("/images/{imagename}")
+    public void imageViewer(HttpServletRequest req,
+            HttpServletResponse res,
+            @PathVariable("imagename") String imageName) {
+        String path = "C:\\Users\\1519m\\Pictures\\";
+        Path image = Paths.get(path, imageName);
+
+        if (Files.exists(image)) {
+            res.setHeader("Content-Disposition", "inline");
+            res.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            try {
+                Files.copy(image, res.getOutputStream());
+                res.getOutputStream().flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+
+            }
+        }
+    }
+
+}
